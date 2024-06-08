43de0ec87c34f9ba1cf05a9d32fadc994e861ff5
josie <josietran.03@gmail.com>
2024-06-07 18:56:00 -0700

    part 2: finished implementing constructor

--- hw1-cmpt225-spring23/src/fifteenpuzzle/FifteenPuzzle.java
+++ hw1-cmpt225-spring23/src/fifteenpuzzle/FifteenPuzzle.java
@@ -21,30 +21,17 @@ public class FifteenPuzzle {
 	 * Reads a board from file and creates the board
 	 */
 	public FifteenPuzzle(String fileName) throws IOException, BadBoardException {
-		// TODO implement me
 		board = new int[SIZE][SIZE];
-		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-			String line;
-			int row = 0;
-			while ((line = br.readLine()) != null && row < 4) {
-				String[] values = line.trim().split("\\s+"); // split by whitespace
-				for (int col = 0; col < values.length && col < 4; col++) {
-					if (!values[col].isEmpty()) {
-						board[row][col] = Integer.parseInt(values[col]);
-					} else {
-						System.out.println("hello empty");
-						board[row][col] = 0;
-					}
-				}
-				row++;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+
+		File file = new File(fileName);
+		if (!file.exists()) {
+			throw new FileNotFoundException("The file " + fileName + " does not exist.");
 		}
-		printBoard();
+		readFromFile(fileName);
+
+		checkBoard();
 	}
 
-	
 	/**
 	 * Get the number of the tile, and moves it to the specified direction
 	 * 
@@ -60,32 +47,72 @@ public class FifteenPuzzle {
 	 * i.e., the board has all tiles in their correct positions
 	 */
 	public boolean isSolved() {
-		// TODO implement me
 		return false;
 	}
 	
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		// TODO implement me
 		StringBuilder result = new StringBuilder();
         for (int[] row : board) {
             for (int j = 0; j < row.length; j++) {
                 int col = row[j];
 				if (col == 0){
-					continue;
+					result.append((char) 32).append((char) 32);
 				}
-                if (col < 10) {
-                    result.append(" ");
-                }
-				result.append(col);
+				else{
+					if (col < 10) {
+						result.append((char) 32);
+					}
+					result.append(col);
+				}
                 if (j < row.length - 1) {
-                    result.append("\t");
+                    result.append((char) 32);
                 }
             }
-            result.append("\n");
+            result.append((char) 10);
         }
 		return result.toString();
+	}
+	private void readFromFile(String fileName){
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			String line;
+			int row = 0;
+			while ((line = br.readLine()) != null && row < SIZE) {
+				String[] values = line.split("(?<=\\G.{3})"); //split the string every 3 characters
+
+				for (int col = 0; col < values.length && col < SIZE; col++) {
+					String value = values[col].trim();
+					if (!value.isEmpty()) {
+						board[row][col] = Integer.parseInt(value);
+					} else {
+						board[row][col] = 0;
+					}
+				}
+				row++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	//void instead of boolean because we want to throw exception at where error occurs
+	private void checkBoard() throws BadBoardException{
+		boolean[] seen = new boolean[SIZE*SIZE];
+
+		for (int[] row : board) {
+			for (int num : row) {
+				if (num < 0 || num >= SIZE*SIZE || seen[num]) { //0 because we store 0 in our 2d array
+					throw new BadBoardException("bad board");
+				}
+				seen[num] = true; // Mark the number as seen
+			}
+		}
+
+		// Check if all numbers from 1 to 15 have been seen
+		for (int i = 0; i <= SIZE*SIZE - 1; i++) {
+			if (!seen[i]) {
+				throw new BadBoardException("missing number");
+			}
+		}
 	}
 	private void printBoard(){
 		for (int i = 0; i < board.length; i++) {
